ab76be1b59fa24d34363002608c3d6a813b2d5fa
julio <jrab997@gmail.com>
2021-05-30 23:07:58 -0500

    firts version

--- script.js
+++ script.js
@@ -28,35 +28,31 @@ const options = {
   }
 };
 let myChart;
-const ctx = document.getElementById('myChart').getContext('2d');
 
-async function getAll(options, ctx) {
+async function getAll(options) {
   try {
     let res = await fetch('./php/read.php');
     let json = await res.json();
     if (!res.ok) throw { status: res.status, statusText: res.statusText };
     options.data.labels = json.names;
     options.data.datasets[0].data = json.stock;
+    const ctx = document.getElementById('myChart').getContext('2d');
     myChart = new Chart(ctx, options);
   } catch (err) {
     console.log(`Error: ${err}`);
   }
 }
 
-document.addEventListener('DOMContentLoaded', getAll(options, ctx));
+document.addEventListener('DOMContentLoaded', getAll(options));
 document.addEventListener('click', (e) => {
   if (e.target.matches('#chart-bar')) {
     myChart.destroy();
     options.type = 'bar';
-    const ctx2 = document.getElementById('myChart2').getContext('2d');
-    getAll(options, ctx2);
+    getAll(options);
   }
   if (e.target.matches('#chart-pie')) {
     myChart.destroy();
     options.type = 'pie';
-    const ctx2 = document.getElementById('myChart2').getContext('2d');
-    getAll(options, ctx2);
+    getAll(options);
   }
 });
-
-const setOptions = () => {};
